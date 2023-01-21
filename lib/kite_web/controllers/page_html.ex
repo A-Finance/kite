f88579eb26a1852c25dd0a96d78841b04f39f6c1
Anirudh <anirudh24seven@gmail.com>
2023-01-21 08:09:31 +0530

    Initial commit

---
+++ lib/kite_web/controllers/page_html.ex
@@ -0,0 +1,5 @@
+defmodule KiteWeb.PageHTML do
+  use KiteWeb, :html
+
+  embed_templates "page_html/*"
+end
---
+++ test/kite_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule KiteWeb.PageControllerTest do
+  use KiteWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  end
+end
